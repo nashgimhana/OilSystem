4edc3a6a9ebb5958ae4fcc83a246c30d8897304c
Buddhika Prasanna <kbuddhika89@gmail.com>
2017-08-13 09:53:08 +0530

    jk

--- src/v/Delivery.java
+++ src/v/Delivery.java
@@ -3309,7 +3309,7 @@ public class Delivery extends javax.swing.JPanel {
             DeliverDetail.getTxtRouteNameVR().setText(txtRouteNameSP.getText());
             DeliverDetail.getTxtVehicleIdVR().setText(txtVehicleIdSP.getText());
             DeliverDetail.getTxtVehicleNumberVR().setText(txtVehicleNameSP.getText());
-            
+
             new v.frmDeliveryInvoice(tblItemDI, txtRouteIdDI, jTabbedPane1).setVisible(true);
         }
 
