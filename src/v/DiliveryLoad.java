d00d4281bb0f913518bbe1e9ffc3d3bf5a0994b1
nadeesha gimhana <nadeesharb@gmail.com>
2018-05-22 19:35:26 +0530

    sa

--- src/v/DiliveryLoad.java
+++ src/v/DiliveryLoad.java
@@ -18,40 +18,22 @@ import pojo.Units;
  * @author Nash
  */
 public class DiliveryLoad {
-    
+
     public void savediliveryload(JTable table, pojo.Delivery d) {
         try {
             for (int i = 0; i < table.getRowCount(); i++) {
                 pojo.DiliveryLoad load = new pojo.DiliveryLoad();
-
-//                pojo.GrnLog grnLog = new GrnLog();
-//                grnLog.setId(Integer.parseInt(itemJTable.getValueAt(i, 3).toString()));
-//                deliveryItemLog.setGrnLog(grnLog);
-//
-//                deliveryItemLog.setDelivery(delivery);
-                Product p = new m.Product().getBy(Integer.parseInt(table.getValueAt(i, 2).toString()));
+                Product p = new m.Product().getByName(table.getValueAt(i, 0).toString());
                 load.setProduct(p);
                 load.setDelivery(d);
                 load.setLoadqty(Double.parseDouble(table.getValueAt(i, 3).toString()));
-                
-                pojo.Units units = new Units();
-                units.setId(new m.Units().getByName01(itemJTable.getValueAt(i, 1).toString()));
-                deliveryItemLog.setUnits(units);
-                
-                deliveryItemLog.setQuantity(Double.parseDouble(itemJTable.getValueAt(i, 2).toString()));
-                
-                int saveLoadItems = new m.DelevrySummery().saveLoadItems(deliveryItemLog);
-                if (saveLoadItems == 1) {
-                    updateAveQty(i);
-                    updateCurrentQty(i);
-                    done = 1;
-                }
+                load.setCrntPrice(p.getCurrentPrice());
+                load.setReturnqty(Double.parseDouble(table.getValueAt(i, 4).toString()));
+                int save = new m.DiliveryLoad().save(load);
             }
-            DefaultTableModel dtm = (DefaultTableModel) itemJTable.getModel();
-            dtm.setRowCount(0);
         } catch (Exception e) {
-            Errormzg.displayerrorMessage(e.toString());
+            e.printStackTrace();
         }
+
     }
-    
 }
